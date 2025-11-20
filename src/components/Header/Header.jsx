fbf469b12c8d77a7c442a8a484cee28ad572767a
alextalijan <acatalijan@icloud.com>
2025-11-20 14:35:12 -0600

    feat: write content for Header that is dependent on login status

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
 import { useContext } from 'react';
 import UserContext from '../../contexts/UserContext';
+import { Link } from 'react-router';
 
 function Header() {
   const { user } = useContext(UserContext);
 
-  return <header></header>;
+  return (
+    <header>
+      {user ? (
+        <>
+          <Link to="/">Chats</Link>
+          <Link to="/account">My Account</Link>
+        </>
+      ) : (
+        <>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Log In</Link>
+        </>
+      )}
+    </header>
+  );
 }
 
 export default Header;
