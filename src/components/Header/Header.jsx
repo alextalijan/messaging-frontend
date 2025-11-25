d7e52207fd60adb1c268313038093a73783cf822
alextalijan <acatalijan@icloud.com>
2025-11-25 15:48:43 -0600

    style: improve looks of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,43 @@
 import { useContext } from 'react';
 import UserContext from '../../contexts/UserContext';
 import { Link } from 'react-router';
+import styles from './Header.module.css';
 
 function Header() {
   const { user, logout } = useContext(UserContext);
 
   return (
-    <header>
-      {user ? (
-        <>
-          <Link to="/">Chats</Link>
-          <Link to="/profile">My Profile</Link>
-          <button type="button" onClick={() => logout()}>
-            Log Out
-          </button>
-        </>
-      ) : (
-        <>
-          <Link to="/register">Register</Link>
-          <Link to="/login">Log In</Link>
-        </>
-      )}
-    </header>
+    <>
+      <header className={styles.header}>
+        {user ? (
+          <>
+            <Link className={styles.link} to="/">
+              Chats
+            </Link>
+            <Link className={styles.link} to="/profile">
+              My Profile
+            </Link>
+            <button
+              className={styles.link}
+              type="button"
+              onClick={() => logout()}
+            >
+              Log Out
+            </button>
+          </>
+        ) : (
+          <>
+            <Link className={styles.link} to="/register">
+              Register
+            </Link>
+            <Link className={styles.link} to="/login">
+              Log In
+            </Link>
+          </>
+        )}
+      </header>
+      <hr />
+    </>
   );
 }
 
