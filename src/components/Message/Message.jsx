aa147abc642168bf274bf073bbeb873979c9f129
alextalijan <acatalijan@icloud.com>
2025-11-26 19:51:25 -0600

    style: add sender dependent styles on messages

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -2,15 +2,33 @@ import UserContext from '../../contexts/UserContext';
 import { useContext } from 'react';
 import styles from './Message.module.css';
 
-function Message({ sender, text }) {
+function Message({ sender, text, anotherMessage }) {
   const { user } = useContext(UserContext);
 
+  const textStyle = {
+    backgroundColor:
+      sender === user.username ? 'var(--primary-color)' : 'white',
+  };
+
+  const messageStyle = {
+    alignSelf: sender === user.username ? 'end' : 'start',
+  };
+
+  const senderStyle = {
+    alignSelf: sender === user.username ? 'end' : 'start',
+    left: sender === user.username ? '-0.5rem' : '0.5rem',
+  };
+
   return (
-    <div className={styles.message}>
-      <p className={styles.text}>{text}</p>
-      <b className={styles.sender}>
-        {sender === user.username ? 'You' : sender}
-      </b>
+    <div className={styles.message} style={messageStyle}>
+      <p className={styles.text} style={textStyle}>
+        {text}
+      </p>
+      {!anotherMessage && (
+        <b className={styles.sender} style={senderStyle}>
+          {sender === user.username ? 'You' : sender}
+        </b>
+      )}
     </div>
   );
 }
