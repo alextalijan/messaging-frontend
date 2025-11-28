a8eb06f72416b8d77ad0002b1e9334e83c324de6
alextalijan <acatalijan@icloud.com>
2025-11-27 20:22:30 -0600

    style: add looks to welcome page

--- src/components/WelcomePage/WelcomePage.jsx
+++ src/components/WelcomePage/WelcomePage.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router';
 import UserContext from '../../contexts/UserContext';
 import { useContext } from 'react';
 import { Navigate } from 'react-router';
+import styles from './WelcomePage.module.css';
 
 function WelcomePage() {
   const { user } = useContext(UserContext);
@@ -11,11 +12,20 @@ function WelcomePage() {
 
   return (
     <>
-      <h1>Welcome to AlexChat!</h1>
-      <p>Please log in or register to start chatting with your friends.</p>
-      <div>
-        <Link to={'/login'}>Log In</Link>
-        <Link to={'/register'}>Register</Link>
+      <h1 className={styles.h1}>Welcome to AlexChat!</h1>
+      <p className={styles['login-msg']}>
+        Please log in or register to start chatting with your friends.
+      </p>
+      <div className={styles['login-links']}>
+        <Link
+          to={'/login'}
+          className={`${styles.link} ${styles['login-link']}`}
+        >
+          Log In
+        </Link>
+        <Link to={'/register'} className={styles.link}>
+          Register
+        </Link>
       </div>
     </>
   );
